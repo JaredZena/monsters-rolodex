42d97ebdd102a2294776e37c30feb482f987082a
Jared Zena <zena.hernandez.010195@gmail.com>
2020-05-16 01:56:05 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
+import './App.css';
+
+class App extends React.Component {
+  
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+  
+  render() {
+    return (
+    <div className="App">
+    <CardList monsters={this.state.monsters}></CardList>
+    </div>
+    );
+  }
+}
+
+export default App;

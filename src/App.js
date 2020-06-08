901f3c41c8edc1c5c07ef3134e45fc93f85dff7a
jared_zn <jared@bankaya.com.mx>
2020-06-07 20:10:19 -0500

    Adding search filter to monster cards

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends React.Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -21,9 +22,15 @@ class App extends React.Component {
   }
   
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
     <div className="App">
-    <CardList monsters={this.state.monsters}></CardList>
+      <input type='search' placeholder='search monsters' onChange={e=>{
+        this.setState({searchField : e.target.value})
+        }}/>
+    <CardList monsters={filteredMonsters}></CardList>
     </div>
     );
   }
